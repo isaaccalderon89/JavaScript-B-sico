f7b63822eb8e2d4129821bba5ff6fad52249bac7
isaac.calderon <isaaccalderon89@gmail.com>
2022-10-03 18:28:00 +0200

    Primer proyecto JS

---
+++ mi-primer-proyecto/index.js
@@ -0,0 +1,11 @@
+//Este es el mensaje de bienvenida 
+console.log("Esta es la puerta de entrada al proyecto")
+
+/*
+    Esta es la parte intermedia de nuestro proyecto 
+    En esta parte realizaremos los chequeos necesarios
+    Por último vamos a la última línea de saludo. 
+*/
+
+//Este va a ser el mansaje que se envía para despedir
+console.log("Adiós")
