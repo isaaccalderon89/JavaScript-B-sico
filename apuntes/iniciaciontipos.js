24b4d38a8e34654dec0481a5e19bc5469b0e017e
isaac.calderon <isaaccalderon89@gmail.com>
2022-10-04 19:19:04 +0200

    Tipos de variable

---
+++ apuntes/iniciaciontipos.js
@@ -0,0 +1,24 @@
+//Tipos primitivos
+
+//NUMBER
+4
+
+//STRING 
+"Hola mundo";
+'Hola mundo';
+`hola mundo`;
+
+//BOOLEANOS 
+true;
+false;
+
+//NULO Y UNDIFINED
+null;
+undefined;
+
+// NULL, UNDIFINED, FALSE, O----- Todas son falsy 
+if (true){
+    console.log("cumple")
+} else {
+    console.log("no cumple")
+}
